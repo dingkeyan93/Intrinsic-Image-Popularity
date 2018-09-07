f0718c0eb73e69ce78d7c013adb480479d95e7a0
dingkeyan93 <dingkeyan93@outlook.com>
2018-09-07 20:13:33 +0800

    upload

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# Instagram popularity
-Intrinsic image popularity on Instagram.
-Wait to add ...
+# Intrinsic image popularity on Instagram
+
+This is a PyTorch implementation of *Intrinsic Image Popularity Prediction: Focusing on the Visual Content*.
+
+run ```python test.py [image_path]``` to evaluate your intrinsic image popularity on Instagram. Please [click here](https://portland-my.sharepoint.com/:u:/g/personal/keyanding2-c_ad_cityu_edu_hk/EeQcNCrMrvRIor44lbj9hOsBN6qZ2SafvdB0auO7TAGchg?e=BxkeuN) to download the released pretrained model. 
+
+Here is a [Web-based Demo](http://keyan.ink/). 
+![Web-based Demo](demo.jpg)
+
+<!-- **Wait to add**:
+1. The raw data of 150 million Instagram post information.
+2. The generated DPIP dataset.  -->
---
+++ test.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import argparse
+import torch
+import torchvision.models
+import torchvision.transforms as transforms
+from PIL import Image
+
+def prepare_image(image):
+    if image.mode != 'RGB':
+        image = image.convert("RGB")
+    Transform = transforms.Compose([
+            transforms.Resize([224,224]),        
+            transforms.ToTensor(),
+            ])
+    image = Transform(image)   
+    image = image[None]
+    return image
+
+def main():
+    image = Image.open(config.image_path)
+    image = prepare_image(image)
+    model = torchvision.models.resnet50()
+    model.fc = torch.nn.Linear(in_features=2048, out_features=1)
+    model.load_state_dict(torch.load('model-resnet-50.pkl')) 
+    model.eval()
+    with torch.no_grad():
+        preds = model(image)
+    score = preds.detach().numpy().item()
+    print('Popularity score: '+str(round(score,2)))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--image_path', type=str, default='images\\0.jpg')
+    config = parser.parse_args()
+    main()
